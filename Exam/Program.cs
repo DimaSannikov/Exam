adb1f00f23091ddf984850262bc52f69f63fcea9
DimaSannikov <dimasannikov@gmail.com>
2023-03-24 21:58:11 +0500

    create array in Exam

---
+++ Exam/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] elementsList = new string[] {"ace", "atom", "go", "back", "add", "bake", "up", "box", "ball", "alarm", "us", "bear", "fox", "do", "after", "cat", "ask", "alone", "bad", "bet", "alloy"};
+
+for (int i = 0; i < elementsList.Length; i++)
+{
+    Console.WriteLine(elementsList[i]);
+}
