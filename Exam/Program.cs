b27a1831a8af78ca56238d0b71ec3615e38fd4ac
DimaSannikov <dimasannikov@gmail.com>
2023-03-24 22:54:27 +0500

    update 01 Exam

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -1,6 +1,26 @@
-﻿string[] elementsList = new string[] {"ace", "atom", "go", "back", "add", "bake", "up", "box", "ball", "alarm", "us", "bear", "fox", "do", "after", "cat", "ask", "alone", "bad", "bet", "alloy"};
+﻿string[] elementsArray = new string[] {"ace", "atom", "go", "back", "add", "bake", "up", "box", "ball", "alarm", "us", "bear", "fox", "do", "after", "cat", "ask", "alone", "bad", "bet", "alloy"};
 
-for (int i = 0; i < elementsList.Length; i++)
+void CheckElementsInArray(string[] array)
 {
-    Console.WriteLine(elementsList[i]);
+    string[] newArray = new string[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        // PrintArray(MakeNewArray(newArray, array[i], i));
+        Console.WriteLine(array[i]);
+    }
 }
+
+// string[] MakeNewArray(string[] array, string element, int index)
+// {
+//     Array.Resize(ref array, array.Length + 1);
+//     array[index] = element;
+//     return array;
+// }
+
+void PrintArray(string[] array)
+{
+    Console.WriteLine("[" + $"{String.Join(", ", array)}" + "]");
+}
+
+PrintArray(elementsArray);
+CheckElementsInArray(elementsArray);
