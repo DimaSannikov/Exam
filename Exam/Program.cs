ad1dadec9054a3ba367954c915fd6c1d6811b0e8
DimaSannikov <dimasannikov@gmail.com>
2023-03-24 23:44:40 +0500

    update 04 Exam

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -1,34 +1,25 @@
 ﻿string[] elementsArray = new string[] {"ace", "atom", "go", "back", "add", "bake", "up", "box", "ball", "alarm", "us", "bear", "fox", "do", "after", "cat", "ask", "alone", "bad", "bet", "alloy"};
 
-void CreateNewArray(string[] array)
+string[] CreateNewArray(string[] array)
 {
     string[] newArray = new string[0];
     for (int i = 0; i < array.Length; i++)
     {
-        // ChangeArrayLength(newArray, array[i]);
-        Array.Resize(ref newArray, newArray.Length + 1);
-        newArray[newArray.Length-1] = array[i];
-        // PrintArray(newArray);
-        // Console.WriteLine(newArray.Length);
-        // Console.WriteLine(array[i]);
+        if (array[i].Length <= 3)
+        {
+            Array.Resize(ref newArray, newArray.Length + 1);
+            newArray[newArray.Length-1] = array[i];
+        }
     }
-    PrintArray(newArray);
-    // Console.WriteLine(newArray.Length);
+    return newArray;
 }
-
-// void ChangeArrayLength(string[] array, string element)
-// {
-//     Array.Resize(ref array, array.Length + 1);
-//     array[array.Length-1] = element;
-//     // return array;
-// }
 
 void PrintArray(string[] array)
 {
     Console.Write("[" + $"{String.Join(", ", array)}" + "]");
 }
 
-// PrintArray(elementsArray);
+PrintArray(elementsArray);
 Console.WriteLine();
-CreateNewArray(elementsArray);
+PrintArray(CreateNewArray(elementsArray));
 Console.WriteLine();
