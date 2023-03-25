2922b2ec0767984aa0fb2f23e92adfdefb2bfecf
DimaSannikov <dimasannikov@gmail.com>
2023-03-25 12:26:05 +0500

    update 05 Exam

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -1,11 +1,11 @@
 ﻿string[] elementsArray = new string[] {"ace", "atom", "go", "back", "add", "bake", "up", "box", "ball", "alarm", "us", "bear", "fox", "do", "after", "cat", "ask", "alone", "bad", "bet", "alloy"};
 
-string[] CreateNewArray(string[] array)
+string[] CreateNewArray(string[] array, int elementLength = 3)
 {
     string[] newArray = new string[0];
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length <= 3)
+        if (array[i].Length <= elementLength)
         {
             Array.Resize(ref newArray, newArray.Length + 1);
             newArray[newArray.Length-1] = array[i];
